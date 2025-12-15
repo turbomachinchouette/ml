614a1814590154c582a578ed3b70f986f61a3f6d
Gabriel Rolland <grp@darkstar.boum.org>
2025-12-15 11:23:02 +0100

    Starting out on VAE_GAN

--- vae_gan/gan.py
+++ vae_gan/gan.py
@@ -34,3 +34,25 @@ test_mnist = torchvision.datasets.MNIST(
 
 # Inputs image tensor, arch Convolutional, Leaky ReLu, output Scalar
 # and intermediate feature layer for feature matching 𝓛.
+
+
+def reparametrize(mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
+    """
+    Samples a normal distribution epsilon to return a tensor z from
+    a variance and a normal distribution tensors.
+    z = mean + standard_dev * epsilon
+    """
+
+    std = torch.exp(0.5 * logvar)
+    eps = torch.randn_like(std)
+    z = mu + std * eps
+    assert z.shape == mu.shape, "Weird things in the shape of these tensors."
+    assert mu.shape == logvar.shape, "Weird things in the shape of these tensors."
+    return z
+
+
+def bce(x_recond_flat, x_flat, reduction="sum"):
+    """
+    This is the binary cross entropy
+    """
+    nn.BCELoss()
